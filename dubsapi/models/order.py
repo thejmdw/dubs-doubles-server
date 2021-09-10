846577886c7aed495c29103778c93b9c9b60b75f
Jonathan Watson <thejmdw@gmail.com>
2021-09-10 15:23:28 -0400

    added customer and order seeds, tweaked order model, changed is_staff to true for user 1

--- dubsapi/models/order.py
+++ dubsapi/models/order.py
@@ -1,4 +1,5 @@
 """Customer order model"""
+from datetime import datetime
 from django.db import models
 from .customer import Customer
 from .payment import Payment
@@ -7,4 +8,4 @@ from .payment import Payment
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.DO_NOTHING,)
     payment_type = models.ForeignKey(Payment, on_delete=models.DO_NOTHING, null=True)
-    created_date = models.DateField(default="0000-00-00",)
+    created_date = models.DateTimeField(default=datetime.now, blank=True)
