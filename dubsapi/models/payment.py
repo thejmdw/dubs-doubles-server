f65e0fb5bad6f2d3b17df4620472041a9de0fbba
Jonathan Watson <thejmdw@gmail.com>
2021-09-22 11:56:54 -0400

    altered payment model account_number field

--- dubsapi/models/payment.py
+++ dubsapi/models/payment.py
@@ -4,7 +4,7 @@ from .customer import Customer
 class Payment(models.Model):
     
     merchant_name = models.CharField(max_length=25)
-    account_number = models.IntegerField()
+    account_number = models.CharField(max_length=16)
     expiration_date = models.CharField(max_length=7)
     customer = models.ForeignKey(Customer, on_delete=models.DO_NOTHING, related_name="payment_types")
     created_on = models.DateField(auto_now_add=True)    
