8f43551e1bb1fa391c941fa84a1c7f126730a1fa
Jonathan Watson <thejmdw@gmail.com>
2021-09-22 12:42:30 -0400

    adjusted payment model

--- dubsapi/models/payment.py
+++ dubsapi/models/payment.py
@@ -7,4 +7,4 @@ class Payment(models.Model):
     account_number = models.CharField(max_length=16)
     expiration_date = models.CharField(max_length=7)
     customer = models.ForeignKey(Customer, on_delete=models.DO_NOTHING, related_name="payment_types")
-    created_on = models.DateField(auto_now_add=True)    
+    created_on = models.DateField(auto_now_add=True)
