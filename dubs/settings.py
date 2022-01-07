554a76874e71ea95c2f0fbe1c78e2f05777bc739
Jonathan Watson <thejmdw@gmail.com>
2022-01-07 12:04:04 -0500

    added CORS ALLOWED ORIGINS

--- dubs/settings.py
+++ dubs/settings.py
@@ -67,9 +67,14 @@ CORS_ORIGIN_WHITELIST = (
     'https://dubs.thejmdw.com',
 )
 
+CORS_ALLOWED_ORIGINS = [
+    "https://dubs.thejmdw.com"
+]
+
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -77,7 +82,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'dubs.urls'
