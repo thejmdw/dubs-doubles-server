a39dc7de69797d24013d2f986791537d3e5d5b69
Jonathan Watson <thejmdw@gmail.com>
2022-01-08 21:59:06 -0500

    removed lines that weren't needed

--- dubs/settings.py
+++ dubs/settings.py
@@ -61,18 +61,10 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10
 }
 
-
-# CORS_ORIGIN_WHITELIST = (
-#     'http://localhost:3000',
-#     'http://127.0.0.1:3000',
-#     # 'http://thejmdw.com',
-#     # 'https://dubs.thejmdw.com/',
-# )
-
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    "https://dubs.thejmdw.com",
+    "https://dubs.thejmdw.com/",
 ]
 
 
