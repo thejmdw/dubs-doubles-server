85429897d1e4c02f9d0de068e70cec48ebe7cb8e
Jonathan Watson <thejmdw@gmail.com>
2022-01-20 18:16:13 -0500

    tweaking daily sales sql query to group by id

--- dubsapi/views/charts.py
+++ dubsapi/views/charts.py
@@ -105,6 +105,7 @@ def dailySales(request):
         query="""
         SELECT
             o.created_date,
+            o.id,
             sum(p.price) AS total_sales
         FROM
             dubsapi_lineitem li
@@ -114,7 +115,7 @@ def dailySales(request):
             dubsapi_product p ON p.id = li.product_id
         JOIN
             dubsapi_customer c ON c.id = o.customer_id
-        GROUP BY o.created_date
+        GROUP BY o.id
         """
         cursor.execute(query)
         row = dictfetchall(cursor)
