84bcb9c3d4f3bba622af4f4adb845acfe0c85cc2
Jonathan Watson <thejmdw@gmail.com>
2022-01-20 19:48:44 -0500

    tweaking daily sales sql query

--- dubsapi/views/charts.py
+++ dubsapi/views/charts.py
@@ -105,7 +105,7 @@ def dailySales(request):
         query="""
         SELECT
             o.created_date,
-
+            o.id,
             sum(p.price) AS total_sales
         FROM
             dubsapi_lineitem li
@@ -115,7 +115,7 @@ def dailySales(request):
             dubsapi_product p ON p.id = li.product_id
         JOIN
             dubsapi_customer c ON c.id = o.customer_id
-        GROUP BY o.created_date
+        GROUP BY o.created_date, o.id
         """
         cursor.execute(query)
         row = dictfetchall(cursor)
